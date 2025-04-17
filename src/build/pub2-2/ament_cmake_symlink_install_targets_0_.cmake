0d320cce07395452a33f38afdf843eda6f4486a2
lsy0727 <92630416+lsy0727@users.noreply.github.com>
2025-04-17 17:12:16 +0900

    Delete src/build directory

--- src/build/pub2-2/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/linux/ros2_ws/src/build/pub2-2/pub2-2" "TARGETS" "pub2-2" "DESTINATION" "lib/pub2-2")
--- src/build/pub2/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/linux/ros2_ws/src/build/pub2/pub2" "TARGETS" "pub2" "DESTINATION" "lib/pub2")
