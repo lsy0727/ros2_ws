dda045d0eaed70d70a3a69f1adf8e0962f62465e
lsy0727 <92630416+lsy0727@users.noreply.github.com>
2025-05-17 19:11:21 +0900

    Update sub_jetson.cpp

--- src/camera1-2/src/sub_jetson.cpp
+++ src/camera1-2/src/sub_jetson.cpp
@@ -7,8 +7,10 @@
 using std::placeholders::_1;
 
 // Windows로 H.264 스트리밍 (테스트 명령어 기반)
-std::string dst = "appsrc ! videoconvert ! video/x-raw,format=I420 ! queue ! x264enc tune=zerolatency bitrate=500 ! rtph264pay pt=96 ! udpsink host=192.168.0.61 port=8001 sync=false";
-
+std::string dst = "appsrc ! videoconvert ! video/x-raw, format=BGRx ! \
+	nvvidconv ! nvv4l2h264enc insert-sps-pps=true ! \
+	h264parse ! rtph264pay pt=96 ! \
+	udpsink host = 203.234.58.121 port = 8001 sync=false";
 cv::VideoWriter writer, outputVideo;    //writer : 스트리밍용, outputVideo : 저장용용
 volatile sig_atomic_t running = 1;
 
