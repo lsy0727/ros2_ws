d841b2a08de89c9726f976b8ff7a50b25923eff8
lsy0727 <92630416+lsy0727@users.noreply.github.com>
2025-06-11 14:17:19 +0900

    Add files via upload

---
+++ src/camera2-2/src/sub_wsl.cpp
@@ -0,0 +1,33 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/compressed_image.hpp"
+#include "opencv2/opencv.hpp"
+#include <memory>
+#include <functional>
+#include <iostream>
+using std::placeholders::_1;
+  
+void mysub_callback(rclcpp::Node::SharedPtr node, const sensor_msgs::msg::CompressedImage::SharedPtr msg)
+{
+    cv::Mat frame = cv::imdecode(cv::Mat(msg->data),  cv::IMREAD_COLOR);
+    cv::imshow("wsl",frame);
+    cv::waitKey(1);
+    RCLCPP_INFO(node->get_logger(), "Received Image : %s,%d,%d", msg->format.c_str(),frame.rows,frame.cols);
+}
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rclcpp::Node>("camsub_wsl");
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort();
+    std::function<void(const sensor_msgs::msg::CompressedImage::SharedPtr msg)> fn;
+    fn = std::bind(mysub_callback, node, _1);
+
+    int fourcc = cv::VideoWriter::fourcc('m', 'p', '4', 'v');
+    double fps = 30;
+    cv::VideoWriter outputVideo("camera2-2.mp4", fourcc, fps, cv::Size(640, 360));
+
+    auto mysub = node->create_subscription<sensor_msgs::msg::CompressedImage>("image/compressed",qos_profile,fn);
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
+
