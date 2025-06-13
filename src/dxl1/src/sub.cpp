a5d0289734ab35ab3e5b0c553276341f7636bdb8
lsy0727 <92630416+lsy0727@users.noreply.github.com>
2025-06-13 12:12:16 +0900

    Update sub.cpp

--- src/dxl1/src/sub.cpp
+++ src/dxl1/src/sub.cpp
@@ -1,34 +1,50 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/vector3.hpp"
-#include "dxl/dxl.hpp"
 #include <memory>
-#include <functional>
-using namespace std::placeholders;
-void mysub_callback(rclcpp::Node::SharedPtr node, Dxl& dxl, const
-geometry_msgs::msg::Vector3::SharedPtr msg)
-{
-    RCLCPP_INFO(node->get_logger(), "Received message: %lf,%lf", msg->x,msg->y);
-    dxl.setVelocity((int)msg->x, (int)msg->y);
-}
-
-int main(int argc, char* argv[])
+int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    Dxl dxl;
-    auto node = std::make_shared<rclcpp::Node>("node_dxlsub");
-    if(!dxl.open())
-    {
-        RCLCPP_ERROR(node->get_logger(), "dynamixel open error");
-        rclcpp::shutdown();
-        return -1;
-    }
+    auto node = std::make_shared<rclcpp::Node>("node_dxlpub");
     auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
-    std::function<void(const geometry_msgs::msg::Vector3::SharedPtr msg)> fn;
-    fn = std::bind(mysub_callback, node, dxl, _1);
-    auto mysub = node->create_subscription<geometry_msgs::msg::Vector3>("topic_dxlpub",qos_profile,fn);
-
-    rclcpp::spin(node);
-    dxl.close();
+    auto mypub = node->create_publisher<geometry_msgs::msg::Vector3>("topic_dxlpub", qos_profile );
+    geometry_msgs::msg::Vector3 vel;
+    vel.x = 0;
+    vel.y = 0;
+    vel.z = 0;
+    rclcpp::WallRate loop_rate(20.0); //20hz->50msec
+    int vel1=0,vel2=0;
+    int goal1=0,goal2=0;
+    while(rclcpp::ok())
+    {
+        char c = 'q';
+        std::cin >> c;
+        if (c != 'q')
+        {
+            switch(c)
+            {
+                case 's': goal1 = 0; goal2 = 0; break;
+                case ' ': goal1 = 0; goal2 = 0; break;
+                case 'f': goal1 = 50; goal2 = -50; break;
+                case 'b': goal1 = -50; goal2 = 50; break;
+                case 'l': goal1 = -50; goal2 = -50; break;
+                case 'r': goal1 = 50; goal2 = 50; break;
+                default : goal1 = 0; goal2 = 0; break;
+            }
+        }
+        
+        // generate accel and decel motion
+        if(goal1>vel1) vel1 += 5;
+        else if(goal1<vel1) vel1 -= 5;
+        else vel1 = goal1;
+        if(goal2>vel2) vel2 += 5;
+        else if(goal2<vel2) vel2 -= 5;
+        else vel2 = goal2;
+        vel.x = vel1;
+        vel.y = vel2;
+        RCLCPP_INFO(node->get_logger(), "Publish: %lf,%lf", vel.x, vel.y);
+        mypub->publish(vel);
+    }
+    loop_rate.sleep();
     rclcpp::shutdown();
     return 0;
 }
